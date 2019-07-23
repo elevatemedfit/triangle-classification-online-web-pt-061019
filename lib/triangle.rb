d5467436fbf5381447c671a81ae528bb62dc89ae
Chris Junker <elevatemedfit@gmail.com>
2019-07-23 20:12:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,29 +1,29 @@
 class Triangle
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
 
-attr_accessor :side1, :side2, :side3
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
 
-def initialize(side1, side2, side3)
-  @side1 = side1
-  @side2 = side2
-  @side3 = side3
+  end
+
 end
-
-def kind()
-   if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-     raise TriangleError
-   elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-     raise TriangleError
-   else
-    return :equilateral if @side1 == @side2 && @side2 == @side3
-    return :isosceles if @side1 == @side2 || @side2 == @side3 || @side1 == @side3
-    :scalene
-end
-end
-
 
 class TriangleError < StandardError
- def message
-   "TriangleError"
- end
-end
+  # triangle error code
 end
