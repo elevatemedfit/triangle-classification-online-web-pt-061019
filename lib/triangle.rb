4b3d2541cca6d44b2f8d6c23c578bffb92b04e77
Chris Junker <elevatemedfit@gmail.com>
2019-07-23 19:30:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,12 @@
-class Triangle
+  class Triangle
 
-  def initialize(side1,side2,side3)
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
-  end#
+  end
 
    def kind(side1,side2,side3)
       if (@side1 + @side2 >= @side3 && @side2 + @side3 >= @side1 && @side3 + @side1 >= @side2) !=true
