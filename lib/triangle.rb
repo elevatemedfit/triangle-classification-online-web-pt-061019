7d76578129e52d0580c07ee65bfc60b80b635d92
Chris Junker <elevatemedfit@gmail.com>
2019-07-23 20:00:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,8 @@ def initialize(side1, side2, side3)
   @side3 = side3
 end
 
- def kind(side1,side2,side3)
-    if (@side1 + @side2 >= @side3 && @side2 + @side3 >= @side1 && @side3 + @side1 >= @side2) == false # ||
+ def kind?(side1,side2,side3)
+    if (@side1 + @side2 >= @side3 && @side2 + @side3 >= @side1 && @side3 + @side1 >= @side2)  # ||
        #(side1 <= 0 || side2 <= 0 || side3 <= 0)
       begin
         raise TriangleError
@@ -25,10 +25,6 @@ end
 end
 end
 end
-
-
-
-
 
 class TriangleError < StandardError
  def message
