fe1b0ebc2db1c1b2905781f6a25f1ad6bdd8c121
Chris Junker <elevatemedfit@gmail.com>
2019-07-23 21:11:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
   attr_accessor :side_1, :side_2, :side_3
-  
+
   def initialize(side_1,side_2,side_3)
     @side_1 = side_1
     @side_2 = side_2
@@ -8,19 +8,19 @@ class Triangle
   end
 
   def kind()
-    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-      begin
-      raise TriangleError::TriangleError
-      rescue TriangleError => error
-      puts error.message
-      end
-    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-      begin
-      raise TriangleError::TriangleError
-      rescue TriangleError => error
-      puts error.message
-      end
-    else
+    raise TriangleError if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      # begin
+      # raise TriangleError::TriangleError
+      # rescue TriangleError => error
+      # puts error.message
+      #end
+    raise TriangleError if (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      # begin
+      # raise TriangleError::TriangleError
+      # rescue TriangleError => error
+      # puts error.message
+      # end
+    #else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
       elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
@@ -32,7 +32,7 @@ class Triangle
 
   end
 
-end
+
 
 class TriangleError < StandardError
 def message
