2da7b33f8e8e8686e7e770ae7e5472904b8ff958
Chris Junker <elevatemedfit@gmail.com>
2019-07-23 20:44:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
   end
 
   def kind()
-    if  side_1 + side_2 > side_3 || side_2 + side_3 > side_1 || side_3 + side_1 > side_2#(@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+    if  side_1 + side_2 > side_3 || side_2 + side_3 > side_1 || side_3 + side_1 > side_2 !=true#(@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       begin #|| (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
     rescue TriangleError => error
