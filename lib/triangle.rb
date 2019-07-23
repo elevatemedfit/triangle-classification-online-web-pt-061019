d6c0b5d611c7f6e76d471c1fc81d3dd4d4436e77
Chris Junker <elevatemedfit@gmail.com>
2019-07-23 18:02:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
 
-  attr_accessor
+  attr_accessor :side1, :side2, :side3
 
   def initialize(side1,side2,side3)
     @side1 = side1
@@ -9,16 +9,26 @@ class Triangle
   end#
 
    def kind(side1,side2,side3)
-     if side1 != side2 && side1 != side3 && side2 != side3 
-     begin
-       raise TriangeError
-     rescue TriangelError => error
-       puts error.message
-     end
-     class TriangleError < StandardError
-       def message
-         "error message"
-       end
+      if (side1+side2> side3 and side2+side3> side1 and side3+side1> side2) != true
+    begin
+        raise PartnerError
+    rescue PartnerError => error
+      puts error.message
+    end
+  else
+    def kind.each do |side1,side2,side|
+      if side1 == side2 && side2 == side3
+        puts "equilateral"
+      elsif side1 == side2 || side2 == side3 || side1 == side3
+        puts "isosceles"
+      elsif side1 != side2 && side1 != side3 && side2 != side3
+        puts "scalene"
+   end
+ end
+
+ class PartnerError < StandardError
+   def message
+     "TriangleError"
    end
  end
  end
