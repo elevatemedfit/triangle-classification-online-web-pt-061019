0bfbcb6c45b1c1993d120e74cff486855eafd99c
Chris Junker <elevatemedfit@gmail.com>
2019-07-23 20:35:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
       || (@side_1+@side_2 <= @side_3)
       || (@side_1+@side_3 <= @side_2)
       || (@side_2+@side_3 <= @side_1)
-      || (@side_1+@side_2+@side_3=0)
+      || (@side_1 + @side_2 + @side_3 = 0)
       raise TriangleError
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
