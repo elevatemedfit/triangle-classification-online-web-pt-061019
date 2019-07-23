9e91b39f5b46370e0ecd5cfc00b92f807c94a144
Chris Junker <elevatemedfit@gmail.com>
2019-07-23 19:57:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ def initialize(side1, side2, side3)
   @side3 = side3
 end
 
- def kind(side1,side2,side3)
+ def kind(a,b,c)
     if ((@side1 + @side2 >= @side3 && @side2 + @side3 >= @side1 && @side3 + @side1 >= @side2) != true) ||
        (side1 <= 0 || side2 <= 0 || side3 <= 0)
       begin
