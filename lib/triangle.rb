21dc3909e8a347c24659cd8c054e4744160b1fc8
Chris Junker <elevatemedfit@gmail.com>
2019-07-23 19:38:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@
   end
 
    def kind
-      if (@side1 + @side2 >= @side3 && @side2 + @side3 >= @side1 && @side3 + @side1 >= @side2) !=true #||(side1 <=0 || side2 <= 0 || side3 <= 0)||(side1 + side2 <= side3)
+      if (@side1 + @side2 >= @side3 && @side2 + @side3 >= @side1 && @side3 + @side1 >= @side2) = false #||(side1 <=0 || side2 <= 0 || side3 <= 0)||(side1 + side2 <= side3)
     begin
         raise PartnerError
     rescue PartnerError => error
