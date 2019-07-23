6db19b963a01042559e67852a5043e3ef5be0a94
Chris Junker <elevatemedfit@gmail.com>
2019-07-23 19:53:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,34 +1,43 @@
-  class Triangle
+class Triangle
 
-  attr_accessor :side1, :side2, :side3
+attr_accessor :side1, :side2, :side3
 
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
-  end
+def initialize(side1, side2, side3)
+  @side1 = side1
+  @side2 = side2
+  @side3 = side3
+end
 
-   def kind
-      if (@side1 + @side2 >= @side3 && @side2 + @side3 >= @side1 && @side3 + @side1 >= @side2) != true #||(side1 <=0 || side2 <= 0 || side3 <= 0)||(side1 + side2 <= side3)
-    begin
+ def kind
+    if (@side1 + @side2 >= @side3 && @side2 + @side3 >= @side1 && @side3 + @side1 >= @side2) != true
+      begin
         raise TriangleError
-    rescue TriangleError => error
-      puts error.message
-    end
+        rescue TriangleError => error
+        puts error.message 
+        end
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+      begin
+        raise TriangleError
+        rescue TriangleError => error
+        puts error.message 
   else
-    # def triangle(a,b,c)
-    #   sides = [a,b,c].sort
-      return :equilateral if @side1 == @side2 && @side2 == @side3
-      return :isosceles if @side1 == @side2 || @side2 == @side3 || @side1 == @side3
-      :scalene
+
+  # def triangle(a,b,c)
+  #   sides = [a,b,c].sort
+    return :equilateral if @side1 == @side2 && @side2 == @side3
+    return :isosceles if @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+    :scalene
+end
+end
 end
 end
 
 
 
- class TriangleError < StandardError
-   def message
-     "TriangleError"
-   end
+
+class TriangleError < StandardError
+ def message
+   "TriangleError"
  end
- end
+end
+end
