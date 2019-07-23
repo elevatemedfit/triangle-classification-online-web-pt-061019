cb1e91395070c745939c99dd13749a17718c6ad4
Chris Junker <elevatemedfit@gmail.com>
2019-07-23 17:55:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
-end
+
+  attr_accessor
+
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end#
+
+   def kind(side1,side2,side3)
+     if side1 != side2 && side1 != side3 && side2 != side3 
+     begin
+       raise TriangeError
+     rescue TriangelError => error
+       puts error.message
+     end
+     class TriangleError < StandardError
+       def message
+         "error message"
+       end
+   end
+ end
+ end
